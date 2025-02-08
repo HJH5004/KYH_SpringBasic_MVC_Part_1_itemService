d7a091618374a827244358df85e1169660868f0e
H0NG <yunjin5004@naver.com>
2025-02-08 13:52:54 +0900

    * 25. 02. 08
    테스트 코드 커밋

--- src/test/java/hello/itemservice/KyhSpringMvcPart1ItemServiceApplicationTests.java
+++ src/test/java/hello/itemservice/KyhSpringMvcPart1ItemServiceApplicationTests.java
@@ -1,13 +1,25 @@
 package hello.itemservice;
 
+import hello.itemservice.domain.item.ItemRepository;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class KyhSpringMvcPart1ItemServiceApplicationTests {
 
+
+    ItemRepository itemRepository = new ItemRepository();
+
+    @AfterEach
+    void clearStore() {
+        itemRepository.clearStore();
+    }
+
+
     @Test
-    void contextLoads() {
+    void save(){
+        as
     }
 
 }
