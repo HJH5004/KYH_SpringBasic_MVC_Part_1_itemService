b6ad972111a2962ea301763c59bdb1251dd4dc4e
Curry5004 <yunjin5004@naver.com>
2025-02-11 07:52:44 +0900

    * 25 02 11
    상품 등록 로직 구현 완료

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -4,13 +4,14 @@ import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @Controller
 @RequestMapping("/basic/items")
 @RequiredArgsConstructor
@@ -24,7 +25,66 @@ public class BasicItemController {
         model.addAttribute("items", allItems);
 
         return "basic/items";
+    }
 
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable("itemId") long itemId, Model model) {
+        Item items = itemRepository.findById(itemId);
+        model.addAttribute("item", items);
+
+        return "basic/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm() {
+        return "basic/addForm";
+    }
+
+//    @PostMapping("/add")
+    public String saveV1(@RequestParam String itemName,
+                       @RequestParam int price,
+                       @RequestParam Integer quantity,
+                       Model model
+    ) {
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+        model.addAttribute("item", item);
+
+        return "basic/item";
+    }
+
+//    @PostMapping("/add")
+    public String saveV2(@ModelAttribute("item") Item item, Model model) {
+
+        log.info("save item: {}", item);
+        itemRepository.save(item);
+
+        //@ModelAttribute를 통해 model에 item이 자동으로 들어가서 생략이 가능 하다.
+//        model.addAttribute("item", item);
+
+        return "basic/item";
+    }
+
+//    @PostMapping("/add")
+    //ModelAttribute에서 value가 생략되어 있으면 관례적으로 모델명 앞글자를 소문자로 하여 던져 준다.
+    //ex Item -> item
+    public String saveV3(@ModelAttribute Item item) {
+
+        itemRepository.save(item);
+
+        return "basic/item";
+    }
+
+    @PostMapping("/add")
+    public String saveV4(Item item) {
+
+        itemRepository.save(item);
+
+        return "basic/item";
     }
 
     @PostConstruct
